315a05ca1d3415aeef8e75ab922a10a3493192dc
Tasnim <tasnim.kheder@esprit.tn>
2026-01-30 22:56:08 +0100

    Workshop 2: Ajout système de gestion des suggestions avec Header, Footer et ListSuggestion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ListSuggestionComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
